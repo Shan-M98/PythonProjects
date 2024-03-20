10ed9934893e7425ffddf6acc956faaa86887c3f
Shan-M98 <122422568+Shan-M98@users.noreply.github.com>
2024-03-20 16:14:02 -0400

    Add files via upload
    My very first python project

---
+++ intials.py
@@ -0,0 +1,22 @@
+my_initials = """
+sss   m   m
+s   s mm mm
+s     mm mm
+ sss  m m m
+    s m   m
+s   s m   m
+ sss  m   m
+# intials.py Project!
+# My name is Shan Majeed and I am starting my first python project!
+"""
+
+print(" sss  m   m")
+print("s   s mm mm")
+print("s     mm mm")
+print(" sss  m m m")
+print("    s m   m")
+print("s   s m   m")
+print(" sss  m   m")
+
+
+
